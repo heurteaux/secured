669e6b3934a70ef2f4e86d4bbc02565fa8e57ccf
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-01-16 18:48:17 +0100

    Added several unit tests concerning hash function

--- tests/hash_testing.c
+++ tests/hash_testing.c
@@ -6,6 +6,8 @@
 */
 
 #include <criterion/criterion.h>
+#include "../lib/my/my.h"
+#include <stdio.h>
 #include "../includes/hashtable.h"
 
 Test(Hash_Test, normal_hash)
@@ -19,4 +21,72 @@ Test(Hash_Test, normal_hash)
     cr_assert(hash2 > 0 && hash2 != hash3);
     cr_assert(hash3 > 0);
     cr_assert(hash4 > 0);
+}
+
+Test(Hash_Test, empty_string)
+{
+    int hash1 = hash("", 1);
+
+    cr_assert(hash1 == -1);
+}
+
+Test(Hash_Test, null_len)
+{
+    int hash1 = hash("voiture", 0);
+
+    cr_assert(hash1 == -1);
+}
+
+Test(Hash_Test, empty_string_null_len)
+{
+    int hash1 = hash("", 0);
+
+    cr_assert(hash1 == -1);
+}
+
+Test(Hash_Test, same_input)
+{
+    int hash1 = hash("voiture", 10);
+    int hash2 = hash("voiture", 10);
+
+    cr_assert_eq(hash1, hash2);
+}
+
+/*char *switch_one_letter(int index, const char *str)
+{
+    char *result = malloc(sizeof(char) * 7);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (i == index)
+            result[i] = 'z';
+        result[i] = str[i];
+    }
+    return result;
+}*/
+/*
+
+Test(Hash_Test, all_data_is_used)
+{
+    char *input = "voiture";
+    char *temp;
+    const int input_len = 7;
+    int *hash_list = malloc(sizeof(int) * 8);
+
+    for (int i = 0; i < input_len; i++) {
+        temp = switch_one_letter(i, input);
+        printf("DEBUG TEST :%s\n", temp);
+        hash_list[i] = hash(temp, 10);
+        free(temp);
+    }
+    hash_list[7] = -1;
+    free(hash_list);
+}
+*/
+
+Test(Hash_Test, small_key_diff)
+{
+    int hash1 = hash("a", 4);
+    int hash2 = hash("b", 4);
+
+    cr_assert(hash1 != hash2);
 }
