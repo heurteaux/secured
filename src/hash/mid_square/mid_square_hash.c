7006680abe3654aa2a94828f61a2bc2200c99b4c
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-01-16 11:42:52 +0100

    Enhanced hash function, now makes use of polynomial rolling on input string and XOR shifting to randomize more function's output

--- src/hash/mid_square/mid_square_hash.c
+++ src/hash/mid_square/mid_square_hash.c
@@ -5,6 +5,10 @@
 ** mid_square_hash.c
 */
 
+#include <limits.h>
+
+const int HSH_LEN = 6;
+
 static long long int power(int power)
 {
     long long int result = 1;
@@ -15,17 +19,25 @@ static long long int power(int power)
     return result;
 }
 
-static long long int key_to_int(const char *key)
+// polynomial rolling key_to_int function using XOR shift to randomize even
+// more output
+static unsigned long int key_to_seed(const char *key, int rdm)
 {
-    long long int result = 0;
+    int base = 37;
+    unsigned long long int result = 0;
+    long long int power = 1;
+    long long int pr_number = 1000000223;
 
     for (int i = 0; key[i] != '\0'; i++) {
-        result += key[i];
+        result = (result * base + (key[i] - 'a' + 1)) % pr_number;
+        result %= LLONG_MAX;
+        power = (power * base) % pr_number;
     }
+    result ^= result << (rdm * rdm);
     return result;
 }
 
-static int get_seed_len(long long int seed)
+static int get_seed_len(unsigned long long int seed)
 {
     int i = 0;
 
@@ -36,22 +48,21 @@ static int get_seed_len(long long int seed)
     return i;
 }
 
-static int get_n_middle_digits(long long int seed, int n)
+static int get_mid_digits(unsigned long long int seed)
 {
     int seed_len = get_seed_len(seed);
-    int i = (seed_len - n) / 2;
+    int i = (seed_len - HSH_LEN) / 2;
+    int len_incr = HSH_LEN - seed_len;
 
-    if (seed_len < n) {
-        while (seed_len < n)
-            n = n / 2 + n % 2;
-    }
-    return (int)((seed / power(i)) % power(n));
+    if (seed_len < HSH_LEN)
+        seed *= power(len_incr);
+    return (int) ((seed / power(i)) % power(HSH_LEN));
 }
 
 int hash(char *key, int len)
 {
-    int seed = key_to_int(key);
+    unsigned long long int seed = key_to_seed(key, len);
 
-    seed *= seed;
-    return get_n_middle_digits(seed, len);
+    seed *= seed % ULLONG_MAX;
+    return get_mid_digits(seed);
 }
