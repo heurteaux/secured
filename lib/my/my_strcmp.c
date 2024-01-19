ac9e37532d2557907d960dd4afd3d4c1462687ff
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-01-19 13:12:32 +0100

    Changed my_strcmp.c

--- lib/my/my_strcmp.c
+++ lib/my/my_strcmp.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2023
-** day06
+** Day06
 ** File description:
 ** task06
 */
@@ -9,12 +9,10 @@
 
 int my_strcmp(char const *s1, char const *s2)
 {
-    int i = 0;
-
-    while (s1[i] == s2[i]) {
-        if (s1[i] == '\0')
-            return 0;
-        i++;
+    for (int i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++) {
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
     }
-    return s1[i] - s2[i];
+    return 0;
 }
